d7d5bc601c1c572db26bf2aace41f733afaa6676
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-12 13:49:44 +0200

    create a VanDetails.jsx file in pages folder, set it up and import to App.jsx, create a route with path /vans/:id

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"; //import 
 import Home from "./pages/Home"; 
 import About from "./pages/About";
 import Vans from "./pages/Vans";
+import VanDetail from "./pages/VanDetails";
 
 
 
@@ -22,6 +23,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
       </Routes>
     </BrowserRouter>
   );
