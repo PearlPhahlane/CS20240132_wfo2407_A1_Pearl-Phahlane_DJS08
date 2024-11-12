6bc5cb16e14cd9d7a153f14a44cc8b2c50500b51
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-12 13:51:39 +0200

    add comment

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route path="/vans/:id" element={<VanDetail />} /> {/*route Params*/}
       </Routes>
     </BrowserRouter>
   );
