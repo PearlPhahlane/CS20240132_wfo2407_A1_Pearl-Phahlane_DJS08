f6bb6c9053e0e02a910823baf5814ef56d94fa09
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-12 11:57:07 +0200

    import react on App jsx file to debug and remove errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react"; //had to import to debug and to use JSX
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"; //import components from react-router-dom
 import Home from "./pages/Home"; 
 import About from "./pages/About";
