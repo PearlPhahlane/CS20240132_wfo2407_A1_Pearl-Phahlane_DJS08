1ae3454d120c0a5485f708ba6dab80eca8afa3bb
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-12 14:33:30 +0200

    fix nav bar with layout route by implementing an outlet

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,12 @@
 import React from "react"
+import { Outlet } from "react-router-dom"
+import Header from "./header"
 
 export default function Layout() {
     return (
-        <h1>This is the Layout Route</h1>
-    )
+      <>
+        <Header />
+        <Outlet />
+      </>
+    );
 }
