8b990d1c945ac8b5e514eba4e8823a5fbef2bcf2
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-12 15:26:36 +0200

    set up routs for dashboard, income and reviews in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,9 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} /> {/*route Params*/}
+          <Route path="/host" element={<Dashboard />} />
+          <Route path="/host/income" element={<Income />} />
+          <Route path="/host/reviews" element={<Reviews />} />
         </Route>
       </Routes>
     </BrowserRouter>
