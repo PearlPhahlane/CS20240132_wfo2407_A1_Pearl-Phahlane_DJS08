542eb66a84e14bcda55f19fdc1363c37d45e1426
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-13 13:13:20 +0200

    add inline active styling with nav link in hostlayout.jsx, replace link with Navlink and add end to host so that the layout navigation links do not get underlined

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,15 +1,41 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+
+const activeStyles = {
+        fontWeight: "bold",
+        textDecoration: "underline",
+        color: "#161616"
+    }
+
 
 export default function HostLayout() {
   return (
     <>
       <nav className="host-nav">
-        <Link to="/host">Dashboard</Link>
-        <Link to="/host/income">Income</Link>
-        <Link to="/host/reviews">Reviews</Link>
-      </nav>
-      <Outlet />
-    </>
+                <NavLink
+                    to="/host"
+                    end
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Dashboard
+                </NavLink>
+
+                <NavLink
+                    to="/host/income"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Income
+                </NavLink>
+
+                <NavLink
+                    to="/host/reviews"
+                    style={({ isActive }) => isActive ? activeStyles : null}
+                >
+                    Reviews
+                </NavLink>
+
+            </nav>
+            <Outlet />
+      </>
   );
 }
