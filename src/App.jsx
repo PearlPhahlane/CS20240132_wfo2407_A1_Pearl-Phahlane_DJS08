f9de6e47e4c773afb9338247ec8fe33ba0aa078d
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-13 17:51:47 +0200

    add a 404 not found page and rounte it

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import HostVanDetail from "./pages/Host/HostVansDetail";
 import HostVanInfo from "./pages/Host/HostVanInfo";
 import HostVanPricing from "./pages/Host/HostVanPricing";
 import HostVanPhotos from "./pages/Host/HostVanPhotos";
+import NotFound from "./pages/Host/NotFound";
 import Layout from "./components/Layout";
 import HostLayout from "./components/HostLayout";
 
@@ -31,12 +32,13 @@ function App() {
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
             <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetail />} >
-              <Route  index element={<HostVanInfo/>} /> 
-              <Route  path="pricing" element={<HostVanPricing/>} /> 
-              <Route  path="photos" element={<HostVanPhotos/>}/> 
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
             </Route>
           </Route>
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
