5b56a3b16f629596f0807926dc0d0199af5d6ff5
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-13 10:37:53 +0200

    add the path to the dashboard in App.jsx, removed the contents in the dashboard.jsx and left it in it's default state

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ function App() {
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} /> {/*route Params*/}
           <Route path="/host" element={<HostLayout />} >
+            <Route path="/host" element={<Dashboard />} />
             <Route path="/host/income" element={<Income />} />
             <Route path="/host/reviews" element={<Reviews />} />
           </Route>
