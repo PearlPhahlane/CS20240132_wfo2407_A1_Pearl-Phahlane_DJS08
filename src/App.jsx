1345c3547eab126625d0a385751c7003786a4b6c
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-13 13:25:48 +0200

    add HostVan detail and HostVans jsx files in the host folder, add place holders to import react in both, add their paths in App.jsx, add navlink for vans in hostlayout.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import VanDetail from "./pages/VanDetails";
 import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
+import HostVans from "./pages/Host/HostVans";
+import HostVanDetail from "./pages/Host/HostVansDetail";
 import Layout from "./components/Layout";
 import HostLayout from "./components/HostLayout";
 
@@ -16,16 +18,18 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path= "/" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} /> {/*route Params*/}
-
-          <Route path="/host" element={<HostLayout />} >
+          
+          <Route path="/host" element={<HostLayout />}>
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />} />
           </Route>
         </Route>
       </Routes>
