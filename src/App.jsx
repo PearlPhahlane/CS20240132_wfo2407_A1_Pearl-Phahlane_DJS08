6f82449254bc5c664ac318549dd58de45216c355
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-13 10:12:01 +0200

    tuen host path into a parent route and nest income and reviews as children routes

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,10 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} /> {/*route Params*/}
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="/host" element={<Dashboard />}>
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
