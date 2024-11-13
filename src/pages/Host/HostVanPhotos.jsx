e3c3c4cdcbbe34a7dd72faaabc6b0e8812d2f2df
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-13 15:17:17 +0200

    add Outlet Context for info, photos and pricing

--- src/pages/Host/HostVanPhotos.jsx
+++ src/pages/Host/HostVanPhotos.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { useOutletContext } from "react-router-dom";
 
 export default function HostVanPhotos() {
-  return <h1>Van Photos page go here</h1>;
+  const { currentVan } = useOutletContext()
+    return (
+        <img src={currentVan.imageUrl} className="host-van-detail-image" />
+    )
 }
--- src/pages/Host/HostVanPricing.jsx
+++ src/pages/Host/HostVanPricing.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { useOutletContext } from "react-router-dom";
 
 export default function HostVanPricing() {
-  return <h1>Pricing page goes here</h1>;
+  const { currentVan } = useOutletContext()
+    return (
+        <h3 className="host-van-price">${currentVan.price}<span>/day</span></h3>
+    )
 }
