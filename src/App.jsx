ed2ffb605af544efeff69334616a5272a43eebcb
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-13 10:26:14 +0200

    create HostLayout component, use link to navigate to dashboard, income and reviews, replace parent host route element with new HostLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
 import Layout from "./components/Layout";
+import HostLayout from "./components/HostLayout";
 
 
 
@@ -20,7 +21,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} /> {/*route Params*/}
-          <Route path="/host" element={<Dashboard />}>
+          <Route path="/host" element={<HostLayout />} >
             <Route path="/host/income" element={<Income />} />
             <Route path="/host/reviews" element={<Reviews />} />
           </Route>
