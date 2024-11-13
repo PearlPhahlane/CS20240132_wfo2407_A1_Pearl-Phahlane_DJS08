ad91d94c92f98d4b4558d2819fdc7954c9b6eeec
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-13 17:47:52 +0200

    remove the vans folder and remove the notfound.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react"; //had to import to debug and to use JSX
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"; //import components from react-router-dom
 import Home from "./pages/Home"; 
 import About from "./pages/About";
-import Vans from "./pages/Vans/Vans";
+import Vans from "./pages/Vans";
 import VanDetail from "./pages/VanDetails";
 import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
@@ -26,7 +26,6 @@ function App() {
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} /> {/*route Params*/}
-
           <Route path="host" element={<HostLayout />}>
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
